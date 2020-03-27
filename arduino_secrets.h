debd404d99ffa77a4f9a76953f3d314ee612415f
ch010402 <christoph.latzer@gmail.com>
2020-03-27 11:49:28 +0100

    added device ID
    added new device byte for device distinction

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,12 +1,22 @@
 const char *appEui = "70B3D57ED002B4FF";
 //mkr_wan_1300-1
-//const char *appKey = "7FD9A69917E6E2DF3CB302B24E5752EF7";
+  /*
+  const uint8_t device[] = { 0x01 };
+  const char *appKey = "7FD9A69917E6E2DF3CB302B24E5752EF7";
+  */
 //mkr_wan_1310-1
-//const char *appKey = "5261E0F3952C63831B419C49A9D0DFE65";
-//uint8_t address[] = { 0x28, 0xF0, 0x22, 0x3A, 0x2D, 0x19, 0x01, 0xC0  };
+  /*
+  const uint8_t device[] = { 0x02 };
+  const char *appKey = "5261E0F3952C63831B419C49A9D0DFE65";
+  uint8_t address[] = { 0x28, 0xF0, 0x22, 0x3A, 0x2D, 0x19, 0x01, 0xC0  };
+  */
 //mkr_wan_1310-2
-//const char *appKey = "0ADF6E6194C88C7C1862FC3FF9A2583B0";
-//uint8_t address[] = { 0x28, 0x48, 0x5D, 0x20, 0x2D, 0x19, 0x01, 0x47  };
+  /*
+  const uint8_t device[] = { 0x03 };
+  const char *appKey = "0ADF6E6194C88C7C1862FC3FF9A2583B0";
+  uint8_t address[] = { 0x28, 0x48, 0x5D, 0x20, 0x2D, 0x19, 0x01, 0x47  };
+  */
 //mkr_wan_1310-3
-const char *appKey = "3BA34667BFBACA7EB9A73BF91D166DCA3";
-uint8_t address[] = { 0x28, 0x48, 0x5D, 0x20, 0x2D, 0x19, 0x01, 0x47  };
+  const uint8_t device[] = { 0x04 };
+  const char *appKey = "3BA34667BFBACA7EB9A73BF91D166DCA3";
+  uint8_t address[] = { 0x28, 0x48, 0x5D, 0x20, 0x2D, 0x19, 0x01, 0x47  };
